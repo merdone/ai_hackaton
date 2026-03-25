407d33df0919c4a35cf15f70dd765cb8d344e427
Mafusail2020 <krutkev00@gmail.com>
2026-03-25 18:23:25 +0200

    feat: Add config class

---
+++ worker/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+class Config:
+    def __init__(self):
+        load_dotenv()
+        self.redis_host = os.getenv("REDIS_HOST", "localhost")
+        self.redis_port = int(os.getenv("REDIS_PORT", 6379))
+        self.redis_db = int(os.getenv("REDIS_DB", 0))
+        self.redis_password = os.getenv("REDIS_PASSWORD", None)
+        
