a1b634f6b81591ad4a457c898f6240632db2ad86
Mafusail2020 <krutkev00@gmail.com>
2026-03-25 18:24:17 +0200

    reafactor: change config file

--- worker/config.py
+++ worker/config.py
@@ -1,12 +1,13 @@
-import os
 from dotenv import load_dotenv
+from os import getenv
+from pydantic import BaseModel
+load_dotenv()
 
 
-class Config:
-    def __init__(self):
-        load_dotenv()
-        self.redis_host = os.getenv("REDIS_HOST", "localhost")
-        self.redis_port = int(os.getenv("REDIS_PORT", 6379))
-        self.redis_db = int(os.getenv("REDIS_DB", 0))
-        self.redis_password = os.getenv("REDIS_PASSWORD", None)
-        
+class Config(BaseModel):
+    DATABASE_PATH: str
+
+
+config = Config(
+    DATABASE_PATH=getenv("DATABASE_PATH")
+)
